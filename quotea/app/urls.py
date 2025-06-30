b386467a095432a1e06cfa6fdefc9a17c0b7aa7c
Raya <rayakuzmina73@gmail.com>
2025-06-30 19:20:15 +0300

    Add basic templates and styles

--- quotea/app/urls.py
+++ quotea/app/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path("", views.home, name="home")
+    path("", views.home, name="home"),
+    path("create_quote", views.create_quote, name="create_quote"),
+    path("popular_quotes", views.popular_quotes, name="popular_quotes"),
 ]
--- quotea/app/views.py
+++ quotea/app/views.py
@@ -1,5 +1,11 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
 
 
 def home(request):
-    return HttpResponse("Hello world!")
+    return render(request, "random_quote.html")
+
+def create_quote(request):
+    return render(request, "create_quote.html")
+
+def popular_quotes(request):
+    return render(request, "popular_quotes.html")
