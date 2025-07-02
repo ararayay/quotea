9c13b1d228e7ee1a1a3ac74d1108c89c716f2024
Raya <rayakuzmina73@gmail.com>
2025-07-02 18:01:45 +0300

    Add ip hashing

--- quotea/app/utils.py
+++ quotea/app/utils.py
@@ -1,3 +1,10 @@
+import hashlib
+
+
+# Функция для хэширования ip
+def hash_ip(ip):
+    return hashlib.sha256(ip.encode('utf-8')).hexdigest()
+
 # Функция для получения IP из запроса
 def get_client_ip(request):
     x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
@@ -5,4 +12,4 @@ def get_client_ip(request):
         ip = x_forwarded_for.split(',')[0]
     else:
         ip = request.META.get('REMOTE_ADDR')
-    return ip
+    return hash_ip(ip)
